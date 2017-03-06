703596c7528d39ddb23713b7dc63015320a5f27a
Tobias Kieslich <tobias.kieslich@gmail.com>
2017-03-06 15:22:09 -0800

    Add redis to datatools

--- 21_datatools.sh
+++ 21_datatools.sh
@@ -1,8 +1,9 @@
 pacman -S --needed --noconfirm --asdeps \
   jre8-openjdk-headless
 pacman -S --needed --noconfirm \
-  postgresql sqlite mongodb elasticsearch rabbitmq
+  postgresql sqlite mongodb elasticsearch rabbitmq redis
 
 usermod -a -G postgres ${USERNAME}
 usermod -a -G rabbitmq ${USERNAME}
 usermod -a -G elasticsearch ${USERNAME}
+usermod -a -G redis ${USERNAME}
