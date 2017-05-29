d8e04929a880b71b94e19655d78255a58442f422
Tobias Kieslich <tobias.kieslich@gmail.com>
2017-05-29 12:33:36 -0700

    Added supend rules for polkit
     - This allows lightdm to make the computer sleep without authorization

--- 12_lightdm.sh
+++ 12_lightdm.sh
@@ -6,3 +6,11 @@ sed -i "s/#session-wrapper/session-wrapper/" /etc/lightdm/lightdm.conf
 
 systemctl enable lightdm.service
 
+cat >> /etc/polkit-1/rules.d/85-suspend.rules << EORULES
+polkit.addRule(function(action, subject) {
+    if (action.id == "org.freedesktop.login1.suspend" &&
+        subject.isInGroup("users")) {
+        return polkit.Result.YES;
+    }
+});
+EORULES
