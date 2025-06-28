aa0f89b04949bbe94ce25aaaf69b5dd2b7d54eb9
Tobias Kieslich <tobias.kieslich@gmail.com>
2025-06-27 18:02:02 -0700

    Add a better working widevine package for aarch64

--- 14_arm_widevine.sh
+++ 14_arm_widevine.sh
@@ -1,8 +1,19 @@
 source config.sh
 source helper.sh
 
+pacman -S --needed --noconfirm ${PACMANEXTRAFLAGS} \
+  squashfs-tools
+
+# the widevine package originates from
+# https://codeberg.org/mogwai/widevine
+#
+# in order to keep Netflix happy, override the useragent inside Mozilla like this:
+#  - call "about:config"
+#  - find or create as 'string' "general.useragent.override"
+#  - set to: "Mozilla/5.0 (X11; CrOS aarch64 15329.44.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
+
 AUR_PACKAGES=(
-  widevine-aarch64
+  widevine
 )
 
 install_aur_packages "${AUR_PACKAGES[@]}"
---
+++ pkgbuilds/widevine/README.md
@@ -0,0 +1,5 @@
+This package provides the widevine library for use with chromium and firefox on x86_64, armv7h and aarch64.
+
+After first installation, you will need to log out and back in. It might also be required to restart the browser up to 2 times before the widevine library is fully recognized and registered.
+
+Please test the setup by browsing to https://bitmovin.com/demos/drm
