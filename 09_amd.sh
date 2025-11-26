1bc0a30d10d44f453e7fa9a2795061def9c50f31
Tobias Kieslich <tobias.kieslich@gmail.com>
2025-11-26 00:20:11 -0800

    Remove amdvlk -> discontinued

--- 09_amd.sh
+++ 09_amd.sh
@@ -2,4 +2,4 @@ source config.sh
 
 pacman -S --needed --noconfirm ${PACMANEXTRAFLAGS} \
   linux-headers linux-firmware \
-  xf86-video-amdgpu vulkan-radeon amdvlk amd-ucode
+  xf86-video-amdgpu vulkan-radeon amd-ucode
